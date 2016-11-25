1990c473d8496b3b5b169ef07df869e3b049c6fd
music960633 <music960633@gmail.com>
2016-11-25 20:52:28 +0800

    add PA3

---
+++ PA3/podem/tdfsim.c
@@ -0,0 +1,20 @@
+/**********************************************************************/
+/*           Parallel-Fault Event-Driven Fault Simulator              */
+/*                                                                    */
+/*           Author: CCM                                              */
+/*           last update : 22/10/2016                                 */
+/**********************************************************************/
+
+#include <stdio.h>
+#include "global.h"
+#include "miscell.h"
+
+//DO YOUR PA#3 CODE HERE
+
+transition_delay_fault_simulation()
+{
+	
+	
+	
+	
+}
